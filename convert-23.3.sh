909ca7e936eefc52db3c62fa52f92826de93e332
Yuji Nakakoshi <yuji.nkks@gmail.com>
2025-07-02 14:24:48 +0900

    add sample schemas 23.3 support

---
+++ convert-23.3.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+sed -i '' 's/bulk_pack_diskettes/cricket/g'    $1
+sed -i '' 's/flat_panel_monitor/baseball/g'    $1
+sed -i '' 's/home_theater_package/tennis/g'    $1
+sed -i '' 's/bookkeeping_application/soccer/g' $1
+sed -i '' 's/printer_supplies/golf/g'          $1
+sed -i '' 's/y_box_games/unknown/g'            $1
+sed -i '' 's/os_doc_set_kanji/misc/g'          $1
+sed -i '' 's/BULK_PACK_DISKETTES/CRICKET/g'    $1
+sed -i '' 's/FLAT_PANEL_MONITOR/BASEBALL/g'    $1
+sed -i '' 's/HOME_THEATER_PACKAGE/TENNIS/g'    $1
+sed -i '' 's/BOOKKEEPING_APPLICATION/SOCCER/g' $1
+sed -i '' 's/PRINTER_SUPPLIES/GOLF/g'          $1
+sed -i '' 's/Y_BOX_GAMES/UNKNOWN/g'            $1
+sed -i '' 's/OS_DOC_SET_KANJI/MISC/g'          $1
